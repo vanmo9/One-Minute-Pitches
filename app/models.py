7f2c3823d9bae40c933da71e5472c5be587480e9
mohassan <vanmowha@gmail.com>
2019-04-20 20:49:17 +0300

    Creating The User model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db 
+
+
+class User(db.Model):
+    __table__= 'users'
+    id = db.Column(db.Interger,primary_key = True)
+    username = db.Column(db.String(255))
+
+
+
+    def __repr__(self):
+        return f'User {self.username}'
