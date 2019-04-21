c006f1f497bc8b5db0cece956449dea3cef4890e
mohassan <vanmowha@gmail.com>
2019-04-21 17:45:05 +0300

    add class

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from . import db
 
 
 class User(db.Model):
-    __table__= 'users'
+    __tablename__= 'users'
     id = db.Column(db.Interger,primary_key = True)
     username = db.Column(db.String(255))
 
