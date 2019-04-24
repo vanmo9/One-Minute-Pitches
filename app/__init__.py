7e33dedf8be9e45b233f8b17145ad657401d6b2b
mohassan <vanmowha@gmail.com>
2019-04-24 09:49:25 +0300

    my project is finally running

--- app/__init__.py
+++ app/__init__.py
@@ -38,6 +38,8 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
+    from .main import main as main_blueprint
+
 
     app.register_blueprint(main_blueprint)
 
@@ -48,7 +50,8 @@ def create_app(config_name):
 #     app.register_blueprint(main_blueprint)
 
     # setting config
-    from .request import configure_request
-    configure_request(app)
+    # from .request import configure_request
+
+    # configure_request(app)
 
     return app
