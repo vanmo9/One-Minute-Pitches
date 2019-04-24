a436127071c0a80778ba6ea85d5c606213abfaf1
mohassan <vanmowha@gmail.com>
2019-04-24 07:35:59 +0300

    add class loginform

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,7 +7,7 @@ from wtforms import ValidationError
 
 
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm): 
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
