2238a071620cb41a00b24b7ff27133c84973dde6
mohassan <vanmowha@gmail.com>
2019-04-24 18:26:35 +0300

    working project

--- app/email.py
+++ app/email.py
@@ -3,7 +3,7 @@ from flask import render_template
 from . import mail
 
 
-#  create sender_email where we store our email address.
+# create sender_email where we store our email address.
 # We then create a mail_message function that takes in 4 parameters which are the subject of the email, the template which is where we create the message body, We pass in the template without an extension because we need to create the text version and a HTML version, the recipient and any keyword arguments.
 def mail_message(subject,template,to,**kwargs):
     sender_email = "vanmowha@gmail.com"
