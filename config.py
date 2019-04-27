56c89d9438ba13b51a206df086759f7579f054ad
mohassan <vanmowha@gmail.com>
2019-04-27 03:22:58 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-   
+    pass
 
 
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MAIL_USERNAME=vanmowha@gmail.com
 export MAIL_PASSWORD=moha
-export SECRET_KEY=moha
+export SECRET_KEY=moha1234
 
 
 python3.6 manage.py server 
